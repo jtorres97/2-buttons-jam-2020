0427a65a3f92065a88c0e1c72d2de3c52394aa7e
Josh <torresj1997@outlook.com>
2020-12-14 17:01:02 -0600

    Restructure project

---
+++ Assets/2BJ2020/Code/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("2BJ2020_Game");
+    }
+
+    public void OpenHighscores()
+    {
+        SceneManager.LoadScene("2BJ2020_Highscores");
+    }
+}
